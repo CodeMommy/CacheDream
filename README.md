f075c510f6b5389549039451dc14d2d326cb63a0
Candison November <kandisheng@163.com>
2016-06-02 00:01:47 +0800

    Update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Mirrors
+# Mirrors.mom
+
+Mirrors.mom is an open source project that provide mirrors service for developers.
--- about/index.php
+++ about/index.php
@@ -1,14 +1,14 @@
 <?php
 $site = array();
-$site['title'] = 'About Mirrors';
-$site['keywords'] = 'About Mirrors,';
-$site['description'] = 'About Mirrors. ';
+$site['title'] = 'About Mirrors.mom';
+$site['keywords'] = 'About Mirrors.mom,';
+$site['description'] = 'About Mirrors.mom. ';
 ?>
 <?php include_once('../include/header.php'); ?>
     <div class="container-fluid">
 
         <div class="well">
-            <h2>About Mirrors</h2>
+            <h2>About Mirrors.mom</h2>
             <p>Mirrors.mom is an open source project that provide mirrors service for developers.</p>
             <p>GitHub: <a target="_blank" title="GitHub" href="https://github.com/ShareAny/Mirrors">https://github.com/ShareAny/Mirrors</a>
             </p>
