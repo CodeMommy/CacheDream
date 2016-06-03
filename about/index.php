9e80996729575857e27c0dd3604d3ffb838a57ad
Candison November <kandisheng@163.com>
2016-06-03 16:57:52 +0800

    Update

--- about/index.php
+++ about/index.php
@@ -14,6 +14,8 @@ $site['description'] = 'About Mirrors.mom. ';
             </p>
         </div>
 
+        <div class="alert alert-success" role="alert">求CDN赞助！求CDN赞助！！求CDN赞助！！！重要的事情说三遍！</div>
+
         <div class="alert alert-warning" role="alert">本网站只提供方便开发者使用的镜像服务，如有镜像侵犯到您的权利，请 <a href="https://github.com/ShareAny/Mirrors/issues" title="联系我们" target="_blank">联系我们</a> 删除镜像。</div>
 
     </div>
