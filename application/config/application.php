6bd7c6bd37910d7ae13415b56bbaa3d9f017ab80
Candison November <kandisheng@163.com>
2017-07-13 21:30:46 +0800

    Update

--- application/config/application.php
+++ application/config/application.php
@@ -5,6 +5,6 @@ use CodeMommy\WebPHP\Environment;
 return array(
     'debug' => Environment::get('application.debug', false),
     'title' => 'CacheDream - an open source project that provide cache service for developers',
-    'domain' => array('staticfile.net', 'www.staticfile.net', 'www.cachedream.com'),
+    'domain' => array('staticfile.net', 'www.staticfile.net', 'mirrors.mom', 'www.mirrors.mom', 'www.cachedream.com'),
     'static' => '//cache.shareany.com/?v=1&f=http://www.cachedream.com/static',
 );
